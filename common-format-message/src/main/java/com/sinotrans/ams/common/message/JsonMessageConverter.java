49e000dfe0b03fbc99bedafdd1c22f33923fd21b
jerry <jerry@xsnake.org>
2019-06-05 18:02:43 +0800

    消除警告

--- common-format-message/src/main/java/com/sinotrans/ams/common/message/JsonMessageConverter.java
+++ common-format-message/src/main/java/com/sinotrans/ams/common/message/JsonMessageConverter.java
@@ -2,7 +2,6 @@ package com.sinotrans.ams.common.message;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
