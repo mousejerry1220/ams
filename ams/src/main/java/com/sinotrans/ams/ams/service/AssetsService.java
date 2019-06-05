4e4f7d5186df3e9a53f6a149840f8949faff4e86
jerry <jerry@xsnake.org>
2019-06-05 15:10:25 +0800

    资产新增

--- ams/src/main/java/com/sinotrans/ams/ams/service/AssetsService.java
+++ ams/src/main/java/com/sinotrans/ams/ams/service/AssetsService.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.sinotrans.ams.common.message.ResponseResult;
 import com.sinotrans.ams.common.message.ResponseSuccess;
@@ -27,7 +28,28 @@ public class AssetsService {
 	 */
 	@RequestMapping("/asset/add")
 	public ResponseResult addService(@RequestBody JSONObject params){
+		
+		//TODO 验证传入的参数合法性的代码
+		
+		//TODO 处理数据的逻辑的代码
+		params.put("ASSET_ID", 1);
+		//插入数据库
+		scriptService.execute("ams_asset_book_add",params);
+		scriptService.execute("ams_asset_card_add",params);
+		scriptService.execute("ams_asset_distribution_add",params);
+		scriptService.execute("ams_asset_other",params);
+		batchExecute(params,"distributionList","");
+		batchExecute(params,"attachmentList","");
+		batchExecute(params,"invoceList","");
+		
 		return new ResponseSuccess(params);
+	}
+	
+	public void batchExecute(JSONObject params,String keys,String scriptName){
+		JSONArray list = params.getJSONArray(keys);
+		for(Object item : list){
+			scriptService.execute(scriptName,(JSONObject)item);
+		}
 	}
 
 	/**
